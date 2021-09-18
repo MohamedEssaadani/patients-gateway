ec690a88f8529e89bf3d5941d3b3a1032c630f8f
Mohamed Es-saadani <essaadani80@gmail.com>
2021-09-18 10:34:57 +0100

    'commit'

---
+++ src/main/java/org/essaadani/patientsgateway/PatientsGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.essaadani.patientsgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientsGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientsGatewayApplication.class, args);
+    }
+
+}
